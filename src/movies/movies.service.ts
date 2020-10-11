8705471ccf77829b5480a04be87ca3ea3c8e95e5
Jerome <jerome@spinprotocol.io>
2020-10-11 23:28:27 +0900

    Validate MovieData by using DTO

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateMovieDTO } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -9,27 +10,27 @@ export class MoviesService {
         return this._movies;
     }
 
-    getOne(id: string): Movie {
-        const movie = this._movies.find(movie => movie.id === +id);
+    getOne(id: number): Movie {
+        const movie = this._movies.find(movie => movie.id === id);
         if (!movie) {
             throw new NotFoundException(`Movie with ID ${id} not found.`);
         }
         return movie;
     }
 
-    deleteOne(id: string) {
+    deleteOne(id: number) {
         this.getOne(id);
-        this._movies = this._movies.filter(movie => movie.id !== +id);
+        this._movies = this._movies.filter(movie => movie.id !== id);
     }
 
-    create(movieData) {
+    create(movieData: CreateMovieDTO) {
         this._movies.push({
             id: this._movies.length + 1,
             ...movieData,
         });
     }
 
-    update(id: string, udpateData) {
+    update(id: number, udpateData) {
         const movie = this.getOne(id);
         this.deleteOne(id);
         this._movies.push({ ...movie, ...udpateData });
